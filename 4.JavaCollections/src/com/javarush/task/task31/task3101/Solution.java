78d041790ad0c9ad33d7da79fb4064b9cfc0ab02
Vladislav Zaitsev <zaycev.vlad.c@gmail.com>
2021-03-08 23:09:48 +0300

    There were some changes in tasks 3101 and 3111

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,48 +1,66 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 /* 
 Проход по дереву файлов
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        File folder = new File(args[0]);
-        File source = new File(args[1]);
-        File destination = new File(source.getParent() + "/allFilesContent.txt");
-        FileUtils.renameFile(source, destination);
-        FileInputStream inputStream;
-        Map<String, String> map = new TreeMap<>();
-        Queue<File> queue = new PriorityQueue<>();
-        Collections.addAll(queue, folder.listFiles());
-
-        while (!queue.isEmpty()){
-            File currentFile = queue.remove();
-            if (currentFile.isDirectory()){
-                Collections.addAll(queue, currentFile.listFiles());
-            }else if(currentFile.length()<=50) {
-                map.put(currentFile.getName(), currentFile.getAbsolutePath());
+    public static void main(String[] args) {
+        String path = args[0];
+        String resultFileAbsolutePath = args[1];
+        try {
+            File resultFile = new File(resultFileAbsolutePath);
+            File dest = new File(resultFile.getParentFile() + "/allFilesContent.txt");
+            if (FileUtils.isExist(dest)) {
+                FileUtils.deleteFile(dest);
             }
+            FileUtils.renameFile(resultFile, dest);
+
+            Map<String, byte[]> fileTree = getFileTree(path);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(dest)) {
+                for (byte[] bytes : fileTree.values()) {
+                    fileOutputStream.write(bytes);
+                    fileOutputStream.write("\n".getBytes());
+                }
+            }
+        } catch (IOException ignored) {
+        }
+    }
+
+    public static Map<String, byte[]> getFileTree(String root) throws IOException {
+        Map<String, byte[]> result = new TreeMap<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private Map<String, byte[]> result;
+
+        public GetFiles(Map<String, byte[]> result) {
+            this.result = result;
         }
 
-
-        FileOutputStream fileOutputStream = new FileOutputStream(destination, true);
-        for (Map.Entry<String, String> value : map.entrySet()){
-            inputStream = new FileInputStream(value.getValue());
-            byte[] buff = new byte[inputStream.available()];
-            int count = inputStream.read(buff);
-            fileOutputStream.write(buff);
-            fileOutputStream.write("\n".getBytes());
-            inputStream.close();
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            File file = path.toFile();
+            if (file.isFile()) {
+                if (file.length() <= 50) {
+                    result.put(path.getFileName().toString(), Files.readAllBytes(path));
+                }
+            }
+            return super.visitFile(path, basicFileAttributes);
         }
-        fileOutputStream.close();
     }
 }
-
--- 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3111/SearchFileVisitor.java
@@ -1,20 +1,62 @@
 package com.javarush.task.task31.task3111;
 
+import com.javarush.task.task31.task3101.Solution;
+
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SearchFileVisitor extends SimpleFileVisitor<Path> {
+    private String name = null;
+    private String partOfContent = null;
+    private int minSize = -1;
+    private int maxSize = -1;
+    private List<Path> result = new ArrayList<>();
 
+    public void setPartOfName(String amigo) { this.name = amigo; }
+    public void setPartOfContent(String programmer) { this.partOfContent = programmer;}
+    public void setMinSize(int i) { this.minSize = i; }
+    public void setMaxSize(int i) { this.maxSize = i; }
+    public List<Path> getFoundFiles() {
+        return result;
+    }
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        byte[] content = Files.readAllBytes(file); // размер файла: content.length
+        byte[] contentSize = Files.readAllBytes(file); // размер файла: content.length
 
+        boolean containsSizeMin = true;
+        if (contentSize.length < 500) {
+            containsSizeMin = false;
+            if (minSize==-1) {
+                containsSizeMin = false;
+            }
+        }
+
+        boolean containsSizeMax = true;
+        if (contentSize.length > 10000) {
+            containsSizeMax = false;
+        }
+
+        boolean containsName = true;
+        if (name != null && !file
+                .getFileName()
+                .toString()
+                .contains(name)) {
+            containsName = false;
+        }
+
+        String contentFile = new String(Files.readAllBytes(file));
+        boolean containsContent = true;
+        if(partOfContent!=null && !contentFile.contains(partOfContent)) {
+            containsContent = false;
+        }
+
+        if(containsName && containsContent && containsSizeMin && containsSizeMax) {
+            result.add(file);
+        }
         return super.visitFile(file, attrs);
     }
+
 }
