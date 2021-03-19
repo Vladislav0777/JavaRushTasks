cb222a43b82fa936cf80f507f6abca9bd2347a7b
Vladislav Zaitsev <zaycev.vlad.c@gmail.com>
2021-03-19 18:23:04 +0300

    Tasks 2208(9, 10, 11, 12) solved

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -9,10 +9,27 @@ import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
+        Map<String, String> paramsMap = new LinkedHashMap<>();
+        paramsMap.put("name", null);
+        paramsMap.put("country", "Ukraine");
+        paramsMap.put("city", "Kiev");
+        paramsMap.put("age", null);
 
+        System.out.println(getQuery(paramsMap));
     }
 
     public static String getQuery(Map<String, String> params) {
-        return null;
+        StringBuilder queryBuilder = new StringBuilder();
+        for (String s : params.keySet()) {
+            String value = params.get(s);
+            if (value == null) {
+                continue;
+            }
+            if (queryBuilder.toString().length() != 0) {
+                queryBuilder.append(" and ");
+            }
+            queryBuilder.append(s + " = '" + value + "'");
+        }
+        return String.valueOf(queryBuilder);
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task22.task2212;
+
+/* 
+Проверка номера телефона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new StringBuilder().append("A").append("b").append("C").toString());
+    }
+}
